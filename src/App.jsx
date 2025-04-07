9f5610b7b7c16a27dbb938cf785486d29b2f2582
arshi <parveenarshi0116@gmail.com>
2025-04-07 16:02:07 +0530

    add edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,18 @@ import expenseData from '../expenseData'
 
 function App() {
   const [expenses, setExpenses] = useState(expenseData)
-
+  const [expense, setExpense] = useState({
+      title: '',
+      category: '',
+      amount: '',
+    })
+    const[rowId,setRowId]=useState('')
   return (
     <main>
       <h1>Track Your Expense</h1>
       <div className="expense-tracker">
-        <ExpenseForm setExpenses={setExpenses} />
-        <ExpenseTable expenses={expenses} setExpenses={setExpenses}  />
+        <ExpenseForm setExpenses={setExpenses} expense={expense} setExpense={setExpense} rowId={rowId}/>
+        <ExpenseTable expenses={expenses} setExpenses={setExpenses}  setExpense={setExpense} setRowId={setRowId}/>
       </div>
     </main>
   )
